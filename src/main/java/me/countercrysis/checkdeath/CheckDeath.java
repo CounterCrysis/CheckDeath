aaa9e9b140e0c9aee0fe7c47c1152d00fee6d1ff
countercrysis <47706960+CounterCrysis@users.noreply.github.com>
2019-02-16 23:44:34 -0700

    Initial Commit

---
+++ src/main/java/me/countercrysis/checkdeath/CheckDeath.java
@@ -0,0 +1,16 @@
+package me.countercrysis.checkdeath;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CheckDeath extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+
+}
