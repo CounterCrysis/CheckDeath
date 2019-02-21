f7bb2ab4e88be2fa9e7b780793b11bc6ca282bef
countercrysis <47706960+CounterCrysis@users.noreply.github.com>
2019-02-21 02:16:00 -0700

    Death Details and Skull

--- src/main/java/me/countercrysis/checkdeath/Events/EventCommand.java
+++ src/main/java/me/countercrysis/checkdeath/Events/EventCommand.java
@@ -9,10 +9,13 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.Plugin;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class EventCommand implements CommandExecutor{
@@ -33,13 +36,14 @@ public class EventCommand implements CommandExecutor{
         }
 
         Player player = (Player) sender;
-        //String uuid     = ys.getUUID(args[0]);
         String username = args[0];
         UUID uuid       = ys.getUUID(username);
         String death    = args[1];
         String data     = (String) ys.get(uuid.toString(), "deaths."+death+".data");
 
+        /*
         Server server = player.getServer();
+
         String w    = (String)  ys.get(uuid.toString(), "deaths."+death+".pos.world");
         Integer x   = (Integer) ys.get(uuid.toString(), "deaths."+death+".pos.x");
         Integer y   = (Integer) ys.get(uuid.toString(), "deaths."+death+".pos.y");
@@ -54,7 +58,20 @@ public class EventCommand implements CommandExecutor{
         System.out.println(player);
         System.out.println(uuid);
         System.out.println(death);
-        //System.out.println(data);
+        System.out.println(data);
+        */
+
+        Map<String, Object> deathDetails = new HashMap<>();
+        deathDetails.put("n",   username);
+        deathDetails.put("t",   death);
+        deathDetails.put("w",   ys.get(uuid.toString(), "deaths."+death+".pos.world"));
+        deathDetails.put("x",   ys.get(uuid.toString(), "deaths."+death+".pos.x"));
+        deathDetails.put("y",   ys.get(uuid.toString(), "deaths."+death+".pos.y"));
+        deathDetails.put("z",   ys.get(uuid.toString(), "deaths."+death+".pos.z"));
+        deathDetails.put("xp",  ys.get(uuid.toString(), "deaths."+death+".xp"));
+        deathDetails.put("msg", ys.get(uuid.toString(), "deaths."+death+".msg"));
+
+
         Inventory inventory;
         try {
             inventory = bs.fromBase64(data);
@@ -64,6 +81,7 @@ public class EventCommand implements CommandExecutor{
         }
 
         ItemStack head = getHead(username, uuid);
+        addDeathDetails(head, deathDetails);
 
         inventory.setItem(inventory.getSize()-1, head);
         player.openInventory(inventory);
@@ -71,13 +89,30 @@ public class EventCommand implements CommandExecutor{
         return true;
     }
 
+    // Translate Color Codes, allowing use of & prefix
+    private static String tcc(String msg) {
+        return ChatColor.translateAlternateColorCodes('&', msg);
+    }
+
+    private static void addDeathDetails(ItemStack item, Map<String, Object> details) {
+        ItemMeta itemMeta = item.getItemMeta();
+        itemMeta.setDisplayName("§c§lDeath " + details.get("n"));
+        Float timeDif = Float.parseFloat((String)details.get("t")) - (System.currentTimeMillis()/1000);
+        itemMeta.setLore(Arrays.asList(
+                tcc("&1World: &r" + details.get("w")),
+                tcc("&9X: &r" + details.get("x") + " &9Y: &r" + details.get("y") + " &9Z: &r" + details.get("z")),
+                tcc("&1Exp: &r" + details.get("xp")),
+                tcc("&9Time: &r" + String.format("%.02f", (timeDif / 60 / 60)) + "h ago"),
+                tcc("&e\"" + details.get("msg") + "\"")
+        ));
+        item.setItemMeta(itemMeta);
+    }
+
     private static ItemStack getHead(String username, UUID uuid) {
         ItemStack itemSkull = new ItemStack(Material.PLAYER_HEAD);
         SkullMeta metaSkull = (SkullMeta) itemSkull.getItemMeta();
-        metaSkull.setDisplayName("§fSkull of " + username);
         metaSkull.setOwningPlayer(Bukkit.getOfflinePlayer(uuid));
         itemSkull.setItemMeta(metaSkull);
-
         return itemSkull;
     }
 
